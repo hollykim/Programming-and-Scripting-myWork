b8e28ffdf6766b1f173490034386f8987603e923
unknown <holly.hyojin@gmail.com>
2024-02-04 20:52:21 +0000

    week2

---
+++ mywork.py
@@ -0,0 +1,22 @@
+# Week 2 lectures practice
+# Author Hyojin Kim 
+
+print ("Hello World")
+
+# math 
+
+floatnum = 0.29
+answer = floatnum
+print (floatnum)
+print (floatnum *100 )
+
+# input 
+
+name = input ("Enter your name: ")
+print ("Hello " + name)
+print ("Hi " + name + "\nNice to meet you")
+
+number = int (input ("Please enter a number: "))
+newNumber = number + 1
+print (f"{number} plus one is {newNumber}")
+
