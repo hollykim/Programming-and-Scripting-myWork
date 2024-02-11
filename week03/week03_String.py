76d771a2b3347dbce212f389acd3ae39f19624ae
Hyojin Kim <holly.hyoijn@gmail.com>
2024-02-11 21:04:27 +0000

    change the folders

---
+++ week03/week03_String.py
@@ -0,0 +1,5 @@
+#lenght.py
+
+inputString = input ("Enther a strin: ")
+lenghtOfString  = len(inputString)
+print(f"The lenght of {inputString} is {lenghtOfString}".format(inputString,lenghtOfString))
