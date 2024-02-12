8798f491fb295ab48f85429eb692edc93d61a915
Hyojin Kim <holly.hyoijn@gmail.com>
2024-02-12 20:09:32 +0000

    week04 my work

---
+++ week04/isEven.py
@@ -0,0 +1,12 @@
+# This program will ask you to enter a number and it will tell you if the number is even or odd.
+# Author: Hyojin Kim
+
+userInput = int(input ("enter a number: "))
+
+if (userInput % 2 == 0):
+        print (f"{userInput} is evne number")
+else: print (f"{userInput} is odd number")
+
+
+#grade.py
+
