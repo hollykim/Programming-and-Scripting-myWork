df84ca89fedef51c39a3625b4642eed8825cccad
Hyojin Kim <holly.hyoijn@gmail.com>
2024-02-21 21:10:57 +0000

    week 05

---
+++ week05/messingWithList.py
@@ -0,0 +1,24 @@
+# Messing with lists
+# AUthor: Hyojin Kim
+
+'''
+fruits = ["mango", "banana", 23, [1,2,3], {}]
+
+print (len (fruits))
+
+fruits = fruits + [1, 2]
+
+print (len (fruits))
+print (fruits)
+
+for fruit in fruits: 
+ print (type(fruit))
+'''    
+
+ages = [1, 2, 3]
+
+sum = 0
+for age in ages:
+   sum += age
+
+print (sum) 
